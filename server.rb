12dfb2dd56f02c0e00bc24c2afa9b2ef163ee77f
Cedric Kisema <cedric.kisema@gmail.com>
2016-10-02 23:09:52 +0100

    adds questionable fact model

--- server.rb
+++ server.rb
@@ -1,6 +1,27 @@
 # server.rb
 require 'sinatra'
+require 'mongoid'
 
+# DB Setup
+Mongoid.load! "mongoid.config"
+
+# Models
+class QuestionableFact
+  include Mongoid::Document
+
+  field :title, type: String
+  field :paragraph, type: String
+  field :fact_id, type: String
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :fact_id, presence: true
+
+  index({ title: 'text' })
+  index({ fact_id: 1 }, { unique: true, name: "fact_index" })
+end
+
+#Routes
 get '/' do
   'Welcome to What the fact!'
 end
