1e17d82d89a6f69f4b1a1d642b07767e59b322bb
Cedric Kisema <cedric.kisema@gmail.com>
2016-10-02 23:49:38 +0100

    adds post route for facts

--- server.rb
+++ server.rb
@@ -51,6 +51,31 @@ namespace '/api/v1' do
     halt(404, { message: 'Fact Not Found'}.to_json) unless fact
     FactSerializer.new(fact).to_json
   end
+
+  post '/facts' do
+    fact = QuestionableFact.new(json_params)
+    if fact.save
+      response.headers['Location'] = "#{base_url}/api/v1/facts/#{fact.fact_id}"
+      status 201
+    else
+      status 422
+      body FactSerializer.new(fact).to_json
+    end
+  end
+
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message: 'Invalid JSON' }.to_json
+      end
+    end
+  end
 end
 
 #Serializers
